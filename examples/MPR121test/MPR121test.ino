4b55850c061dee4741d16081daa4edf43058f04c
vicente95650 <vicentec@brotherswindows.com>
2021-06-20 15:28:23 -0700

    Update MPR121test.ino

--- examples/MPR121test/MPR121test.ino
+++ examples/MPR121test/MPR121test.ino
@@ -1,17 +1,17 @@
 /*********************************************************
 This is a library for the MPR121 12-channel Capacitive touch sensor
 
-Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
+Designed specifically to work with the MPR121 Breakout in the Adafruit shop
   ----> https://www.adafruit.com/products/
 
-These sensors use I2C communicate, at least 2 pins are required 
+These sensors use I2C communicate, at least 2 pins are required
 to interface
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
 products from Adafruit!
 
-Written by Limor Fried/Ladyada for Adafruit Industries.  
+Written by Limor Fried/Ladyada for Adafruit Industries.
 BSD license, all text above must be included in any redistribution
 **********************************************************/
 
@@ -19,7 +19,7 @@ BSD license, all text above must be included in any redistribution
 #include "Adafruit_MPR121.h"
 
 #ifndef _BV
-#define _BV(bit) (1 << (bit)) 
+#define _BV(bit) (1 << (bit))
 #endif
 
 // You can have up to 4 on one i2c bus but one is enough for testing!
@@ -34,11 +34,11 @@ void setup() {
   Serial.begin(9600);
 
   while (!Serial) { // needed to keep leonardo/micro from starting too fast!
-    delay(10);
+    delay(30);
   }
-  
-  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
-  
+
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test");
+
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap.begin(0x5A)) {
@@ -50,12 +50,16 @@ void setup() {
 
 void loop() {
   // Get the currently touched pads
+  //  When the filtered reading is more than 12 counts smaller than the baseline, the chip reports a touch.
   currtouched = cap.touched();
-  
+
   for (uint8_t i=0; i<12; i++) {
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
       Serial.print(i); Serial.println(" touched");
+      Serial.print("Base: ");Serial.println(cap.baselineData(i));  // added by vvc
+      Serial.print("Filt: ");Serial.println(cap.filteredData(i));   // added by vvc
+      Serial.print("difference: "); Serial.println(cap.baselineData(i)-cap.filteredData(i)); //added by vvc
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
@@ -68,7 +72,7 @@ void loop() {
 
   // comment out this line for detailed data from the sensor!
   return;
-  
+
   // debugging info, what
   Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
   Serial.print("Filt: ");
@@ -81,7 +85,7 @@ void loop() {
     Serial.print(cap.baselineData(i)); Serial.print("\t");
   }
   Serial.println();
-  
+
   // put a delay so it isn't overwhelming
   delay(100);
 }
